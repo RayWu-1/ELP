916fe52f34438a07b994a260bbe1a1493de03e8a
fuelatank <fuelatank@gmail.com>
2024-11-13 20:50:34 +0800

    Add SchoolMap and RoundButton components with basic styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { SchoolMap } from './SchoolMap';
 
 // The Main page, where user can select between
 function Main() {
@@ -19,8 +20,26 @@ function Main() {
           Learn React
         </a>
       </header>
+      <SchoolMap backgroundMap="logo512.png" rooms={[
+        {
+          position: {
+            x: 0.4,
+            y: 0.4,
+          },
+          demandLevel: "dirty",
+          onClick: () => console.log("clicked 1"),
+        },
+        {
+          position: {
+            x: 0.6,
+            y: 0.6,
+          },
+          demandLevel: "normal",
+          onClick: () => console.log("clicked 2"),
+        }
+      ]} />
     </div>
   );
 }
 
-export default App;
+export default Main;
---
+++ src/SchoolMap.js
@@ -0,0 +1,18 @@
+import { RoundButton } from "./RoundButton";
+import "./SchoolMap.css";
+
+export function SchoolMap(props) {
+    const { backgroundMap, rooms } = props;
+    return (
+        <div className="school-map-container">
+            {/* Background Map Image */}
+            <img src={backgroundMap} alt="School Map" className="school-map-background" />
+
+            {/* Clickable Rooms */}
+            {rooms.map((room, index) => {
+                const { position, demandLevel, onClick } = room;
+                return <RoundButton position={position} demandLevel={demandLevel} onClick={onClick} key={index} />;
+            })}
+        </div>
+    );
+}
