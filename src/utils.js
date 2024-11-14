2a7e7349fb918afcb1e9be8faf3d7174ca5a7134
fuelatank <fuelatank@gmail.com>
2024-11-14 19:26:10 +0800

    separate config.js and utils.js

---
+++ src/utils.js
@@ -0,0 +1,26 @@
+export function demandLevelColor(level) {
+    switch (level) {
+        case "good":
+            return "green";
+        case "somewhat-dirty":
+            return "yellow";
+        case "dirty":
+            return "red";
+        default:
+            return "black";
+    }
+}
+
+export function demandFrequencyLevel(freq) {
+    if (freq === 0) {
+        return "good";
+    } else if (freq < 3) {
+        return "somewhat-dirty";
+    } else {
+        return "dirty";
+    }
+}
+
+export function genderIconOfId(id) {
+    return id[2] === "M" ? "male.svg" : "female.svg";
+}
