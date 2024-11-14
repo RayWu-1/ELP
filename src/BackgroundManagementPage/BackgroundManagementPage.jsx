e7d177535a6f161b4df18d7bdb1a48d93a7ead98
fuelatank <fuelatank@gmail.com>
2024-11-14 17:04:59 +0800

    Remove unused echarts imports

--- src/BackgroundManagementPage/BackgroundManagementPage.jsx
+++ src/BackgroundManagementPage/BackgroundManagementPage.jsx
@@ -1,9 +1,3 @@
-import * as echarts from 'echarts/core';
-import { BarChart } from 'echarts/charts';
-// 按需引入标题，提示框组件
-import { TitleComponent, TooltipComponent } from 'echarts/components';
-// 引入 Canvas 渲染器
-import { CanvasRenderer } from 'echarts/renderers';
 import Chart from "./chart";
 import { useEffect, useRef, useState } from "react";
 import axios from "axios";
