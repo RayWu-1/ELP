543485874ad893281731756b8a4c824974f4fbc1
StanH123h <1735443634@qq.com>
2024-11-14 16:41:01 +0800

    feat BackgroundManagementPage: added page to manage background info

---
+++ src/BackgroundManagementPage/BackgroundManagementPage.jsx
@@ -0,0 +1,54 @@
+import * as echarts from 'echarts/core';
+import {BarChart} from 'echarts/charts';
+// 按需引入标题，提示框组件
+import {TitleComponent, TooltipComponent} from 'echarts/components';
+// 引入 Canvas 渲染器
+import {CanvasRenderer} from 'echarts/renderers';
+import Chart from "./chart";
+import {useEffect, useRef, useState} from "react";
+import axios from "axios";
+
+export const BackgroundManagementPage = () => {
+    const BASE_URL="http://localhost:8085/demand"
+    const [sortedFrequencies,setSortedFrequencies]=useState([])
+    const sortedFrequenciesRef=useRef([])
+    const [options,setOptions]=useState({})
+    useEffect(() => {
+        axios.get(BASE_URL+"/frequencies/all").then(res=>{
+            setSortedFrequencies(res.data.sort(function(a,b){
+                return a.period-b.period;
+            }))
+        })
+    }, []);
+    useEffect(() => {
+        sortedFrequenciesRef.current = sortedFrequencies;
+        setOptions( {
+            tooltip: {},
+            legend: {
+                data: sortedFrequenciesRef.current.map((frequency)=>frequency.toiletId),
+            },
+            xAxis: {
+                data: [8,9,10,11,12,13,14,15,16,17],
+            },
+            yAxis: {
+            },
+            series: sortedFrequenciesRef.current.map((frequency)=>{
+                return (
+                    {
+                        name: frequency.toiletId,
+                        type: "line",
+                        data: Object.values(frequency.frequencies),
+                    }
+                )
+            }),
+        })
+        console.log(sortedFrequenciesRef.current)
+    }, [sortedFrequencies]); // 监听 sortedFrequencies 变化
+
+    return (
+        <>
+            <Chart options={options} name={"所有不同时间段反馈数据"}/>
+            
+        </>
+    )
+}
--- src/CurrentSituationPage/CurrentSituationPage.jsx
+++ src/CurrentSituationPage/CurrentSituationPage.jsx
@@ -1,5 +1,27 @@
 import "./CurrentSituationPage.scss"
+import {useEffect, useState} from "react";
+import axios from "axios";
 export const CurrentSituationPage=()=>{
+    const BASE_URL="http://localhost:8085/demand"
+    const [timer,setTimer] = useState(0);
+    const [data, setData]=useState(null)
+    useEffect(() => {
+        axios.get(BASE_URL+"/all").then(res=>{
+            setData(res.data)
+            console.log(res.data)
+        })
+        const intervalId = setInterval(() => {
+
+                axios.get(BASE_URL+"/all").then(res=> {
+                    setData(res.data)
+                })
+
+        }, 1000*20);
+
+// 清除定时器
+        return () => clearInterval(intervalId);
+    }, []); // 空依赖数组表示只在组件挂载和卸载时执行一次
     return(<>
+        {data?data.map((dat)=>(<div>{dat.id}</div>)):<></>}
     </>)
 }
